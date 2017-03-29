6fbda53bd8a2f45e647e3094fe26e19ad6bf190b
FKNoble <f.k.noble@massey.ac.nz>
2017-03-30 11:43:45 +1300

    Saved and loaded model.

--- saving_and_loading/main.py
+++ saving_and_loading/main.py
@@ -9,8 +9,8 @@ import tensorflow as tf
 # Create placeholders as empty variables, which you send data to when evaluating the graph.
 # The name_scope groups objects together. Here, the inputs A and B are in the 'input' name_scope.
 with tf.name_scope('input'):
-    A = tf.placeholder(tf.float32, None, 'A')
-    B = tf.placeholder(tf.float32, None, 'B')
+    A = tf.placeholder(tf.float32, shape=None, name='A')
+    B = tf.placeholder(tf.float32, shape=None, name='B')
 
     # Here, we create scalar summaries, which record A's and B's values.
     tf.summary.scalar('A', A)
@@ -19,6 +19,9 @@ with tf.name_scope('input'):
 # Creates a simple graph, which adds two tensors.
 # Here, the add operation and the output, C are in the 'output' name_scope.
 with tf.name_scope('output'):
+
+    C = tf.Variable(0, name='C')
+
     C = A + B
 
     #Here, we record C's value
@@ -48,3 +51,18 @@ with tf.Session() as s:
 
     # To visualise output via TensorBoard, run: 'tensorboard --logdir=./logs'
     # and browse to, by default, 'localhost:6006'.
+
+    # Save model to './model/'.
+    saver = tf.train.Saver()
+    saver.save(s, './model/main-0')
+    saver.export_meta_graph('./model/main.meta')
+
+with tf.Session() as l:
+
+    # Load the model from './model'
+    loader = tf.train.import_meta_graph('./model/main.meta')
+    loader.restore(l, './model/main-0')
+
+    ans = l.run(C, {A: 2.0, B: 5.0})
+
+    print(ans)
