ce2cbae633d574691cde3eec3815e0f199b284ca
FKNoble <f.k.noble@massey.ac.nz>
2017-04-05 16:59:46 +1200

    Working on train.py. Not quite there yet.

--- path_segmentation/train.py
+++ path_segmentation/train.py
@@ -92,7 +92,7 @@ record_file = output_dir + 'training_record.tfrecords'
 
 # Here, we define the number of times we read a record file, and what size
 # each batch is.
-num_epochs = 1000
+num_epochs = 1
 batch_size = 1
 
 # Here, we create handles for reading and writing TFRecord files.
@@ -102,7 +102,7 @@ record = input_pipeline([record_file], num_epochs, batch_size)
 with tf.name_scope('input'):
 
     x = tf.placeholder(tf.float32, shape=[None, None, None, None], name='x')
-    y = tf.placeholder(tf.float32, shape=[None, None, None, None], name='y')
+    y = tf.placeholder(tf.int32, shape=[None, None, None, None], name='y')
 
 # Here, we define our graph.
 with tf.name_scope('network'):
@@ -110,19 +110,270 @@ with tf.name_scope('network'):
     token = x
 
     with tf.variable_scope('conv1_1'):
+
         kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
-                                 shape=[3, 3, 3, 3], name='kernel')
-        bias = tf.get_variable(initializer=tf.constant_initializer(0), shape=[3], name='bias')
+                                 shape=[3, 3, 3, 64], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[64], name='bias')
         conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
         token = tf.nn.bias_add(conv, bias)
-        
+
+    with tf.variable_scope('relu1_1'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv1_2'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 64, 64], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[64], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu1_2'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('pool1'):
+
+        token = tf.nn.avg_pool(token, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    with tf.variable_scope('conv2_1'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 64, 128], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[128], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu2_1'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv2_2'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 128, 128], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[128], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu2_2'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('pool2'):
+
+        token = tf.nn.avg_pool(token, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    with tf.variable_scope('conv3_1'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 128, 256], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[256], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu3_1'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv3_2'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 256, 256], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[256], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu3_2'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv3_3'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 256, 256], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[256], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu3_3'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv3_4'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 256, 256], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[256], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu3_4'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('pool3'):
+
+        token = tf.nn.avg_pool(token, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    with tf.variable_scope('conv4_1'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 256, 512], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[512], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu4_1'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv4_2'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 512, 512], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[512], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu4_2'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv4_3'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 512, 512], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[512], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu4_3'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv4_4'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 512, 512], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[512], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu4_4'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('pool4'):
+
+        token = tf.nn.avg_pool(token, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    with tf.variable_scope('conv5_1'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 512, 512], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[512], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu5_1'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv5_2'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 512, 512], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[512], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu5_2'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv5_3'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 512, 512], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[512], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu5_3'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('conv5_4'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[3, 3, 512, 512], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[512], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu5_4'):
+
+        token = tf.nn.relu(token)
+
+    with tf.variable_scope('pool5'):
+
+        token = tf.nn.avg_pool(token, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    with tf.variable_scope('conv6_1'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[7, 7, 512, 4096], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[4096], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu6_1'):
+
+        token = tf.nn.relu(token)
+        token = tf.nn.dropout(token, 0.5)
+
+    with tf.variable_scope('conv7_1'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[1, 1, 4096, 4096], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[4096], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu7_1'):
+
+        token = tf.nn.relu(token)
+        token = tf.nn.dropout(token, 0.5)
+
+    with tf.variable_scope('conv8_1'):
+
+        kernel = tf.get_variable(initializer=tf.truncated_normal_initializer(0, 0.2),
+                                 shape=[1, 1, 4096, 4096], name='kernel')
+        bias = tf.get_variable(initializer=tf.constant_initializer(0.1), shape=[4096], name='bias')
+        conv = tf.nn.conv2d(token, kernel, strides=[1, 1, 1, 1], padding='SAME')
+        token = tf.nn.bias_add(conv, bias)
+
+    with tf.variable_scope('relu8_1'):
+
+        token = tf.nn.relu(token)
+        token = tf.nn.dropout(token, 0.5)
+
+    with tf.variable_scope('reshape'):
+        shape = tf.shape(x)
+        token = tf.image.resize_images(token, [shape[1], shape[2]])
+
 # Here, we define our output
 with tf.name_scope('Output'):
 
     y_ = token
 
-cost = tf.losses.mean_squared_error(y, y_)
-optimizer = tf.train.AdamOptimizer(1e-2).minimize(cost)
+# cost = tf.losses.mean_squared_error(y, y_)
+cost = tf.reduce_mean((tf.nn.sparse_softmax_cross_entropy_with_logits(
+    logits=y_,
+    labels=tf.squeeze(y, squeeze_dims=[3]))))
+optimizer = tf.train.AdamOptimizer(1e-4).minimize(cost)
 tf.summary.scalar('Cost', cost)
 
 # Initialisation commands
@@ -140,19 +391,23 @@ with tf.Session() as l:
 
     saver = tf.train.Saver()
 
+    model_dir = 'E:/Users/fknoble/My_FCN/' # for very large models.
+
     try:
-        loader = tf.train.import_meta_graph('./model/main.meta')
-        ckpt = tf.train.latest_checkpoint('./model/')
+        loader = tf.train.import_meta_graph(model_dir + 'model/main.meta')
+        ckpt = tf.train.latest_checkpoint(model_dir + 'model/')
         loader.restore(l, ckpt)
     except IOError as e:
-        saver.save(l, './model/main.ckpt', 0)
-        saver.export_meta_graph('./model/main.meta')
+        saver.save(l, model_dir + '/model/main.ckpt', 0)
+        saver.export_meta_graph(model_dir + 'model/main.meta')
 
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
 
     try:
         i = 0
+
+        print('Step ' + str(i))
 
         while not coord.should_stop():
 
@@ -163,11 +418,12 @@ with tf.Session() as l:
 
             summary_writer.add_summary(summary, i)
 
-            if i % (num_epochs / 10) == 0:
-                print(c)
+            if i % 1 == 0:
+                print('Step ' + str(i))
+                print('Cost: ' + c)
 
             if i % 50 == 0:
-                saver.save(l, './model/main.ckpt', i)
+                saver.save(l, model_dir + 'model/main.ckpt', i)
 
             i += 1
 
