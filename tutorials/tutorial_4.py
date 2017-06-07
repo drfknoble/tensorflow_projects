8819cb033c9d5a9e3141e964ae25747acb0f91ae
FKNoble <f.k.noble@massey.ac.nz>
2017-06-07 17:23:29 +1200

    Minor tweaks.

--- tutorials/tutorial_4.py
+++ tutorials/tutorial_4.py
@@ -7,17 +7,20 @@
 import os
 import tensorflow as tf
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # suppresses warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # suppresses warnings
 
 x = tf.placeholder(dtype=tf.float32, shape=[None, 1], name='x')
 y = tf.placeholder(dtype=tf.float32, shape=[None, 1], name='y')
 
+
 def mlp_layer(in_x, w_shape, b_shape):
     '''mlp_layer'''
-    W = tf.get_variable(name='W', shape=w_shape, dtype=tf.float32, initializer=tf.random_uniform_initializer(minval=0.1, maxval=1))
-    b = tf.get_variable(name='b', shape=b_shape, dtype=tf.float32, initializer=tf.random_uniform_initializer(minval=0.1, maxval=1))
 
-    # out_y = W*in_x + b
+    W = tf.get_variable(name='W', shape=w_shape, dtype=tf.float32,
+                        initializer=tf.random_uniform_initializer(minval=0.1, maxval=1))
+    b = tf.get_variable(name='b', shape=b_shape, dtype=tf.float32,
+                        initializer=tf.random_uniform_initializer(minval=0.1, maxval=1))
+
     out_y = tf.add(tf.matmul(in_x, W), b)
 
     return out_y
@@ -40,19 +43,12 @@ with tf.Session() as s:
     s.run(init)
 
     for i in range(0, 100, 1):
-        # l, _ = s.run([loss, training], feed_dict={x : [1.0], y: [2.0]})
-        a = [1.0]
-        b = [2.0]
 
-        a = s.run(tf.reshape(a, [-1, 1]))
-        b = s.run(tf.reshape(b, [-1, 1]))
+        l, _ = s.run([loss, training], feed_dict={x: [[1.0]], y: [[2.0]]})
 
-        l, _ = s.run([loss, training], feed_dict={x : a, y: b})
-
-        if i%10 == 0:
+        if i % 10 == 0:
             print(l)
 
     print(s.run(["layer_1/W:0", "layer_1/b:0"]))
 
     print(s.run(y_, feed_dict={x: [[1.0]]}))
-
