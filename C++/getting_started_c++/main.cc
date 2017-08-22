f2fc5d88f39c3f504631317c6233eae0fa4aea2f
Noble <fknoble@massey.ac.nz>
2017-08-23 11:00:30 +1200

    Current update.

---
+++ C++/getting_started_c++/main.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "tensorflow\core\public\session.h"
+
+int main(int argc, char* argv[]) {
+
+	tensorflow::Session* sess;
+
+	
+		
+
+	system("pause");
+
+	return 0;
+}
